86002272253f18626574551ef64cb772333d6b0e
Mohit.jk <mohit.jk@gmail.com>
2025-12-01 21:00:22 +0000

    fsd

--- blink-of-an-eye/functions/youtube.ts
+++ blink-of-an-eye/functions/youtube.ts
@@ -1,9 +1,10 @@
 export async function onRequest(context: EventContext<Env, any, any>) {
   const apiKey = context.env.YOUTUBE_API_KEY;
   const channelId = "UCWuk3YTnT5-9QxeoVF-wUFw"; // your channel ID
+  const uploadsPlaylist = "PL_6j7S4y5BHgMWvFWghGwAZsOrCQFe-9e"; // uploads playlist ID
 
   const res = await fetch(
-    `https://www.googleapis.com/youtube/v3/search?order=date&type=videos&videoDuration=long&part=snippet&channelId=${channelId}&maxResults=4&key=${apiKey}`
+    `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${uploadsPlaylist}&channelId=${channelId}&maxResults=4&key=${apiKey}`
   );
   const data = await res.json();
 
